a6907c1a052b833833a00f3a49dad1c91d49dbc5
Wing Lee <winglee6@163.com>
2024-08-26 15:01:07 +0800

    update 240826

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,5 @@ SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 echo $SCRIPTPATH
 
-g++ -std=c++11 $SCRIPTPATH/../utils/render_balls_so.cpp -o $SCRIPTPATH/../utils/render_balls_so.so -shared -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+# g++ -std=c++11 $SCRIPTPATH/../utils/render_balls_so.cpp -o $SCRIPTPATH/../utils/render_balls_so.so -shared -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 /Users/lee/Git\ Projects/pointnet.pytorch/utils/render_balls_so.cpp -o /Users/lee/Git\ Projects/pointnet.pytorch/utils/render_balls_so.so -shared -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0
